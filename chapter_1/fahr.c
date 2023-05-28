33149b5e8cd9d45e90592943141001530d1d02d0
fpetrakov <jakephin2015@gmail.com>
2023-05-28 12:04:05 +0300

    restrucrure + 1.3 exercise

--- chapter_1/fahr.c
+++ chapter_1/fahr.c
@@ -15,9 +15,10 @@ int main() {
 	step = 20;
 
 	fahr = lower;
+	printf("Temperatures: \n");
 	while(fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
